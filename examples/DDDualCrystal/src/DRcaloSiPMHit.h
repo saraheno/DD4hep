24f89b6f6b5ffebd941e406fef99e109e3543147
Sarah <sarah.eno@cern.ch>
2021-04-28 19:30:20 -0400

    sd for dr cal

---
+++ examples/DDDualCrystal/src/DRcaloSiPMHit.h
@@ -0,0 +1,65 @@
+#ifndef DRcaloSiPMHit_h
+#define DRcaloSiPMHit_h 1
+
+#include "DRsimInterface.h"
+
+#include "G4VHit.hh"
+#include "G4THitsCollection.hh"
+#include "G4Allocator.hh"
+#include "G4ThreeVector.hh"
+
+#include "DD4hep/Objects.h"
+#include "DD4hep/Segmentations.h"
+
+namespace ddDRcalo {
+  class DRcaloSiPMHit : public G4VHit {
+  public:
+
+    DRcaloSiPMHit(G4int wavBin, G4int timeBin);
+    DRcaloSiPMHit(const DRcaloSiPMHit &right);
+    virtual ~DRcaloSiPMHit();
+
+    const DRcaloSiPMHit& operator=(const DRcaloSiPMHit &right);
+    G4bool operator==(const DRcaloSiPMHit &right) const;
+
+    inline void *operator new(size_t);
+    inline void operator delete(void* aHit);
+
+    virtual void Draw() {};
+    virtual void Print() {};
+
+    void photonCount() { fPhotons++; }
+    unsigned long GetPhotonCount() const { return fPhotons; }
+
+    void SetSiPMnum(dd4hep::DDSegmentation::CellID n) { fSiPMnum = n; }
+    const dd4hep::DDSegmentation::CellID& GetSiPMnum() const { return fSiPMnum; }
+
+    void CountWavlenSpectrum(DRsimInterface::hitRange range);
+    const DRsimInterface::DRsimWavlenSpectrum& GetWavlenSpectrum() const { return fWavlenSpectrum; }
+
+    void CountTimeStruct(DRsimInterface::hitRange range);
+    const DRsimInterface::DRsimTimeStruct& GetTimeStruct() const { return fTimeStruct; }
+
+  private:
+    dd4hep::DDSegmentation::CellID fSiPMnum;
+    unsigned long fPhotons;
+    DRsimInterface::DRsimWavlenSpectrum fWavlenSpectrum;
+    DRsimInterface::DRsimTimeStruct fTimeStruct;
+    G4int fWavBin;
+    G4int fTimeBin;
+  };
+
+  typedef G4THitsCollection<DRcaloSiPMHit> DRcaloSiPMHitsCollection;
+  extern G4ThreadLocal G4Allocator<DRcaloSiPMHit>* DRcaloSiPMHitAllocator;
+
+  inline void* DRcaloSiPMHit::operator new(size_t) {
+    if (!DRcaloSiPMHitAllocator) DRcaloSiPMHitAllocator = new G4Allocator<DRcaloSiPMHit>;
+    return (void*)DRcaloSiPMHitAllocator->MallocSingle();
+  }
+
+  inline void DRcaloSiPMHit::operator delete(void*aHit) {
+    DRcaloSiPMHitAllocator->FreeSingle((DRcaloSiPMHit*) aHit);
+  }
+}
+
+#endif
---
+++ examples/DDDualCrystal/src/DRcaloSiPMSD.cpp
@@ -0,0 +1,101 @@
+#include "DRcaloSiPMSD.h"
+#include "DRcaloSiPMHit.h"
+
+#include "DDG4/Geant4Mapping.h"
+#include "DDG4/Geant4VolumeManager.h"
+
+#include "G4HCofThisEvent.hh"
+#include "G4SDManager.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4ParticleTypes.hh"
+
+#include "G4SystemOfUnits.hh"
+#include "DD4hep/DD4hepUnits.h"
+
+ddDRcalo::DRcaloSiPMSD::DRcaloSiPMSD(const std::string aName, const std::string aReadoutName, const dd4hep::Segmentation& aSeg)
+: G4VSensitiveDetector(aName), fHitCollection(0), fHCID(-1),
+fWavBin(120), fTimeBin(600), fWavlenStart(900.), fWavlenEnd(300.), fTimeStart(10.), fTimeEnd(70.)
+{
+  collectionName.insert(aReadoutName);
+  fSeg = dynamic_cast<dd4hep::DDSegmentation::GridDRcalo*>( aSeg.segmentation() );
+  fWavlenStep = (fWavlenStart-fWavlenEnd)/(float)fWavBin;
+  fTimeStep = (fTimeEnd-fTimeStart)/(float)fTimeBin;
+}
+
+ddDRcalo::DRcaloSiPMSD::~DRcaloSiPMSD() {}
+
+void ddDRcalo::DRcaloSiPMSD::Initialize(G4HCofThisEvent* hce) {
+  fHitCollection = new ddDRcalo::DRcaloSiPMHitsCollection(SensitiveDetectorName,collectionName[0]);
+  if (fHCID<0) { fHCID = GetCollectionID(0); }
+  hce->AddHitsCollection(fHCID,fHitCollection);
+}
+
+G4bool ddDRcalo::DRcaloSiPMSD::ProcessHits(G4Step* step, G4TouchableHistory*) {
+  if(step->GetTrack()->GetDefinition() != G4OpticalPhoton::OpticalPhotonDefinition()) return false;
+
+  auto theTouchable = step->GetPostStepPoint()->GetTouchable();
+
+  dd4hep::sim::Geant4VolumeManager volMgr = dd4hep::sim::Geant4Mapping::instance().volumeManager();
+  dd4hep::VolumeID volID = volMgr.volumeID(theTouchable);
+
+  G4ThreeVector global = step->GetPostStepPoint()->GetPosition();
+  G4ThreeVector local = theTouchable->GetHistory()->GetTopTransform().TransformPoint( global );
+  dd4hep::Position loc(local.x() * dd4hep::millimeter/CLHEP::millimeter, local.y() * dd4hep::millimeter/CLHEP::millimeter, local.z() * dd4hep::millimeter/CLHEP::millimeter);
+  dd4hep::Position glob(global.x() * dd4hep::millimeter/CLHEP::millimeter, global.y() * dd4hep::millimeter/CLHEP::millimeter, global.z() * dd4hep::millimeter/CLHEP::millimeter);
+
+  auto cID = fSeg->cellID(loc, glob, volID);
+
+  G4int nofHits = fHitCollection->entries();
+  G4double hitTime = step->GetPostStepPoint()->GetGlobalTime();
+  G4double energy = step->GetTrack()->GetTotalEnergy();
+
+  ddDRcalo::DRcaloSiPMHit* hit = NULL;
+
+  for (G4int i = 0; i < nofHits; i++) {
+    if ( (*fHitCollection)[i]->GetSiPMnum()==cID ) {
+      hit = (*fHitCollection)[i];
+      break;
+    }
+  }
+
+  if (hit==NULL) {
+    hit = new DRcaloSiPMHit(fWavBin,fTimeBin);
+    hit->SetSiPMnum(cID);
+
+    fHitCollection->insert(hit);
+  }
+
+  hit->photonCount();
+
+  DRsimInterface::hitRange wavRange = findWavRange(energy);
+  hit->CountWavlenSpectrum(wavRange);
+
+  DRsimInterface::hitRange timeRange = findTimeRange(hitTime);
+  hit->CountTimeStruct(timeRange);
+
+  return true;
+}
+
+DRsimInterface::hitRange ddDRcalo::DRcaloSiPMSD::findWavRange(G4double en) {
+  int i = 0;
+  for ( ; i < fWavBin+1; i++) {
+    if ( en < wavToE( (fWavlenStart - (float)i*fWavlenStep)*dd4hep::nm ) ) break;
+  }
+
+  if (i==0) return std::make_pair(fWavlenStart,99999.);
+  else if (i==fWavBin+1) return std::make_pair(0.,fWavlenEnd);
+
+  return std::make_pair( fWavlenStart-(float)i*fWavlenStep, fWavlenStart-(float)(i-1)*fWavlenStep );
+}
+
+DRsimInterface::hitRange ddDRcalo::DRcaloSiPMSD::findTimeRange(G4double stepTime) {
+  int i = 0;
+  for ( ; i < fTimeBin+1; i++) {
+    if ( stepTime < ( (fTimeStart + (float)i*fTimeStep)*CLHEP::ns ) ) break;
+  }
+
+  if (i==0) return std::make_pair(0.,fTimeStart);
+  else if (i==fTimeBin+1) return std::make_pair(fTimeEnd,99999.);
+
+  return std::make_pair( fTimeStart+(float)(i-1)*fTimeStep, fTimeStart+(float)i*fTimeStep );
+}
