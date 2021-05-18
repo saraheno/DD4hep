f1025cfd96a5fb918d50324be799914e125146ce
Sarah <sarah.eno@cern.ch>
2021-05-17 22:48:45 -0400

    hate geometry

--- examples/DDDualCrystal/src/DRSimple_geo.cpp
+++ examples/DDDualCrystal/src/DRSimple_geo.cpp
@@ -70,10 +70,14 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector s
   std::cout<<" theta range is "<<thetamax<<std::endl;
   double zmaxt = outer_r*tan(thetamax);
   std::cout<<" zlength at top is "<<zmaxt<<std::endl;
+  double zmaxm = (outer_r/2.)*tan(thetamax);
+  std::cout<<" zlength at middle is "<<zmaxm<<std::endl;
   double delzb=zmaxb/nzdiv;
   std::cout<<" z div at bottom "<<delzb<<std::endl;
   double delzt=zmaxt/nzdiv;
   std::cout<<" z div at top "<<delzt<<std::endl;
+  double delzm=zmaxm/nzdiv;
+  std::cout<<" z div at middle "<<delzm<<std::endl;
 
   // these refer to different fields in the xml file for this detector
   xml_comp_t fX_struct( x_det.child( _Unicode(structure) ) );
@@ -106,31 +110,41 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector s
 
   // detector element for a tower. need to make a volume, a placed volume, and then associate the placed volume to this detector element
 
-  DetElement tower_det("tower0",det_id);  // detector element for a tower
 
 
 
 
 
-  //  for(int itower=0;itower<nzdiv;itower++) {
-  for(int itower=0;itower<1;itower++) {
 
-    double aatheta = atan((itower+0.5)*delzb/inner_r);
+  for(int itower=0;itower<nzdiv;itower++) {
+  //for(int itower=nzdiv-1;itower<nzdiv;itower++) {
 
 
-    // since all towers in a phi ring are identical, make volumes here
+    std::cout<<"ITOWER is "<<itower<<std::endl;
+
+    if((itower==0)||(itower==nzdiv-1)) {
+
+    //each z division has a unique tower
+    string t_name = _toString(itower,"tower%d");
+    DetElement tower_det(t_name,det_id);  // detector element for a tower
+
+    // angle for tower at this z division with respect to x-y plane
+    double aatheta = atan((itower*(delzt-delzb))/thick)*180./M_PI;
+
+
+
 
 
   /*  ROOT:TGeoTrap
 It has 11 parameters: 
 the half length in z, 
-the 2 polar angles from the centre of the face at low z to that at high z, 
+the 2 polar angles from the centre of the face at low z to that at high z, (polar along x, polar along y)
 H1 the half length in y at low z, 
 LB1 the half length in x at low z and y low edge, 
 LB2 the half length in x at low z and y high edge, 
 TH1 the angle w.r.t. the y axis from the centre of low y edge to the centre of the high y edge, and H2, LB2, LH2, TH2, the corresponding quantities at high z
   */
-    std::cout<<"making trap "<<std::endl;
+    std::cout<<"making trap for eta "<<itower<<std::endl;
     std::cout<<"half thickness "<<(thick+0.1)/2.<<std::endl;
     std::cout<<"polar angle "<<0.<<std::endl;
     std::cout<<"polar angle "<<aatheta<<" "<<std::endl;
@@ -138,11 +152,21 @@ TH1 the angle w.r.t. the y axis from the centre of low y edge to the centre of t
     std::cout<<"half length in x at low z and y low edge "<<delzb<<" "<<std::endl;
     std::cout<<"half length in x at low z and y high edge "<<delzb<<" "<<std::endl;
     std::cout<<"polar angle "<<0.<<" "<<std::endl;
-    std::cout<<"half length in y at low z"<<outphil<<" "<<std::endl;
+    std::cout<<"half length in y at low z "<<outphil<<" "<<std::endl;
     std::cout<<"half length in x at low z and y low edge "<<delzt<<" "<<std::endl;
     std::cout<<"half length in x at low z and y high edge "<<delzt<<" "<<std::endl;
     std::cout<<"polar angle "<<0.<<std::endl;
-    dd4hep::Trap tower((thick+0.1)/2.,0.,aatheta,inphil,delzb,delzb,0.,outphil,delzt,delzt,0.);
+    dd4hep::Trap towertrap((thick)/2.,aatheta,0.,inphil/2.,delzb/2.,delzb/2.,0.,outphil/2.,delzt/2.,delzt/2.,0.);
+
+
+    // needs a hole for fiber
+    dd4hep::Tube fiberhole = dd4hep::Tube(0.,fX_core.rmax(),thick);
+    // rotationzyx.  first a rotation of an angle phi around the z axis followed by a rotation of an angle theta around the y axis followed by a third rotation of an angle psi around the x axis (phi, theta,psi)
+    Transform3D tra(RotationZYX(0,aatheta,0.),Translation3D(0.,0.,0.));
+
+    dd4hep::SubtractionSolid tower(towertrap,fiberhole,tra);
+
+
     dd4hep::Volume towerVol( "tower", tower, description.material(fX_barrel.materialStr()) );
     if(itower==0) std::cout<<"    material is "<<fX_barrel.materialStr()<<std::endl;
     towerVol.setVisAttributes(description, fX_barrel.visStr());
@@ -151,56 +175,72 @@ TH1 the angle w.r.t. the y axis from the centre of low y edge to the centre of t
 
     // fibers
 
-    dd4hep::Tube fiber = dd4hep::Tube(0.,fX_core.rmin(),thick);
+    /*
+        dd4hep::Tube fiber = dd4hep::Tube(0.,fX_core.rmin(),thick);
+
     std::cout<<" making fiber from "<<fX_core.materialStr()<<std::endl;
     dd4hep::Volume coreVol("core", fiber, description.material(fX_core.materialStr()));
+
+    if ( fX_core.isSensitive() ) {
+            coreVol.setSensitiveDetector(sens);
+    }
+
 
     DetElement afiber(tower_det,"fiber",det_id);
     afiber.setAttributes(description,coreVol,fX_core.regionStr(),fX_core.limitsStr(),fX_core.visStr());
 
 
+
     PlacedVolume fiber_phv = towerVol.placeVolume( coreVol, Position(0.,0.,thick/2. ));
     fiber_phv.addPhysVolID("fiber",1);
     afiber.setPlacement(fiber_phv);
-    
- 
 
-
+    */
 
     // now do the placement
 
     double mod_x_off = 0.;             
     double mod_y_off = inner_r + thick/2;  
+    double mod_z_off= delzm/2.;
 
 
-    //for (int nPhi = 0; nPhi < nphi; nPhi++) {
     for (int nPhi = 0; nPhi < 1; nPhi++) {
+    //std::cout<<"starting phi loop"<<std::endl;
+    //for (int nPhi = 0; nPhi < nphi; nPhi++) {
       double phi=nPhi*delphi;
+      std::cout<<"placing at phi "<<phi<<std::endl;
 
       double m_pos_x = mod_x_off * std::cos(phi) - mod_y_off * std::sin(phi);
       double m_pos_y = mod_x_off * std::sin(phi) + mod_y_off * std::cos(phi);
+      //double m_pos_x = mod_y_off * std::cos(phi);
+      //double m_pos_y =  mod_y_off * std::sin(phi);
+      double m_pos_z = mod_z_off+1.0*(delzm*itower);
+      //double m_pos_x = 0.;
+      //double m_pos_y = 0.;
+      //double m_pos_z = mod_z_off+1.0*(delzm*itower);
 
 
 
-
-      Transform3D tr(RotationZYX(0,phi,M_PI*0.5),Translation3D(-m_pos_x,-m_pos_y,0.));
+      //Transform3D tr(RotationZYX(0,phi,M_PI*0.5),Translation3D(m_pos_x,m_pos_y,m_pos_z));
+      Transform3D tr(RotationZYX(-M_PI*0.5,phi,M_PI*0.5),Translation3D(-m_pos_x,-m_pos_y,m_pos_z));
       PlacedVolume pv = envelope.placeVolume(towerVol,tr);
       pv.addPhysVolID("system",det_id);
       pv.addPhysVolID("barrel",0);
+      pv.addPhysVolID("ieta",itower);
+      pv.addPhysVolID("module",nPhi+1);
 
-      int imod=nPhi+itower*nphi;
-      std::cout<<"making itower nphi phi imod "<<itower<<" "<<nPhi<<" "<<phi<<" "<<imod<<std::endl;
-      pv.addPhysVolID("module",imod+1);
 
-	DetElement sd = ((nPhi==0)&&(itower==0)) ? tower_det : tower_det.clone(_toString(imod,"tower0%d"));
+      DetElement sd = nPhi==0 ? tower_det : tower_det.clone(t_name+_toString(nPhi,"0%d"));
+
+
       sd.setPlacement(pv);
       sdet.add(sd);
 
 
     }
 
-
-  }
+      }
+      } // end tower loop
 
 
 
